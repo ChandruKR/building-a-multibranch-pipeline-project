977d317df7173cd9396f660f18986618d5a85aae
ChandruKR <114970167+ChandruKR@users.noreply.github.com>
2025-03-13 06:55:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React<br/>
+          This is a new line I added under Development Branch running under port 3000
         </a>
       </header>
     </div>
